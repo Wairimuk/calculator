e968a5c3ae854b89dffd26ccd2e76cd6682f3904
Wairimu Karuga <wairimukaruga4@gmail.com>
2021-02-18 21:22:46 +0300

    add a division function

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,35 @@
-let add = function(number1, number2){
-  return number1 + number2;
-  };
+// let add = function(number1, number2){
+//   return number1 + number2;
+//   };
 
-  let number1 = parseInt(prompt("Enter a number:"));
-  let number2 = parseInt(prompt("Enter another number:"));
-  alert("The result is " + add(number1, number2));
+//   let number1 = parseInt(prompt("Enter a number:"));
+//   let number2 = parseInt(prompt("Enter another number:"));
+//   alert("The result is " + add(number1, number2));
   
-  let subtract = function(number1, number2){
-    return number1 - number2;
-    };
+//   let subtract = function(number1, number2){
+//     return number1 - number2;
+//     };
   
-    let number1 = parseInt(prompt("Enter a number:"));
-    let number2 = parseInt(prompt("Enter another number:"));
-    alert("The result is " + subtract(number1, number2));
+//     let number1 = parseInt(prompt("Enter a number:"));
+//     let number2 = parseInt(prompt("Enter another number:"));
+//     alert("The result is " + subtract(5, 3));
 
-    let multiply = function(number1, number2){
-      return number1 * number2;
-    }
+//     let multiply = function(number1, number2){
+//       return number1 * number2;
+//       };
 
-    let number1 = parseInt(prompt("Enter a number:"));
-    let number2 = parseInt(prompt("Enter another number:"));
-    alert("The result is " + multiply(number1, number2))
+//       let number1 = parseInt(prompt("Enter a number:"));
+//       let number2 = parseInt(prompt("Enter another number:"));
+//       let result = number1 * number2;
+//       alert("The result is " + result);
+
+      let division = function(firstNumber, secondNumber){
+      return number1 / number2;
+      };
+
+      let firstNumber = parseInt(prompt("Enter a number:"));
+      let secondNumber = parseInt(prompt("Enter another number"));
+      let result = firstNumber/secondNumber;
+      alert("The result is " + result);
+
 
