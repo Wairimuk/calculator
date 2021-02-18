b06cad2eea7b0160b537a2bd124cb3fe9938f0f4
Wairimu Karuga <wairimukaruga4@gmail.com>
2021-02-18 14:26:01 +0300

    add a multiply function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 let add = function(number1, number2){
   return number1 + number2;
-  }
-  
+  };
+
   let number1 = parseInt(prompt("Enter a number:"));
   let number2 = parseInt(prompt("Enter another number:"));
   alert("The result is " + add(number1, number2));
   
+  let subtract = function(number1, number2){
+    return number1 - number2;
+    };
+  
+    let number1 = parseInt(prompt("Enter a number:"));
+    let number2 = parseInt(prompt("Enter another number:"));
+    alert("The result is " + subtract(number1, number2));
+
+    let multiply = function(number1, number2){
+      return number1 * number2;
+    }
+
+    let number1 = parseInt(prompt("Enter a number:"));
+    let number2 = parseInt(prompt("Enter another number:"));
+    alert("The result is " + multiply(number1, number2))
+
