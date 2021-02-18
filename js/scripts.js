82519b84be70cf5f42f766536f2bd10e9cf6f789
Wairimu Karuga <wairimukaruga4@gmail.com>
2021-02-18 13:38:53 +0300

    add an initial index and js files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let add = function(number1, number2){
+  return number1 + number2;
+  }
+  
+  let number1 = parseInt(prompt("Enter a number:"));
+  let number2 = parseInt(prompt("Enter another number:"));
+  alert("The result is " + add(number1, number2));
+  
